c0155f92cf90f40e0dbaf7dd5e27bf09054f8e73
Ioannis Tzomiadis <itzomiad@emeal.nttdata.com>
2024-07-23 18:10:08 +0100

    secrets

--- data/database.js
+++ data/database.js
@@ -5,34 +5,21 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-if (!clusterAddress || !dbUser || !dbPassword || !dbName) {
-  console.error('Missing required environment variables:');
-  console.error(`MONGODB_CLUSTER_ADDRESS: ${clusterAddress}`);
-  console.error(`MONGODB_USERNAME: ${dbUser}`);
-  console.error(`MONGODB_PASSWORD: ${dbPassword}`);
-  console.error(`MONGODB_DB_NAME: ${dbName}`);
-  process.exit(1);
-}
-
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
-const connectToDatabase = async () => {
-  try {
-    await client.connect();
-    await client.db(dbName).command({ ping: 1 });
-    console.log('Connected successfully to server');
-    return client.db(dbName);
-  } catch (error) {
-    console.error('Connection failed:', error.message);
-    await client.close();
-    console.log('Connection closed.');
-    process.exit(1); // Exit process with failure
-  }
-};
+try {
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
+  console.log('Connected successfully to server');
+} catch (error) {
+  console.log('Connection failed.');
+  await client.close();
+  console.log('Connection closed.');
+}
 
-const database = await connectToDatabase();
+const database = client.db(dbName);
 
 export default database;
